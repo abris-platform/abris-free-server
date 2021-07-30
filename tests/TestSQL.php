9aaff5afa72c045e9f1b037f2c1d886bfa4bd977
Dmitrii Ryzhov <ryzhov@abrisplatform.ru>
2021-07-30 16:12:44 +0300

    Repaire TestSQL.php

--- tests/TestSQL.php
+++ tests/TestSQL.php
@@ -4,24 +4,43 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-require_once dirname(__FILE__) . "/../Server/db/DatabasePostgresql.php";
+require_once dirname(__FILE__) . '/../Server/db/DatabasePostgresql.php';
 
 class TestSQL extends TestCase
 {
-    public function test_sql(){
+    protected function connectDB() {
+        global $_STORAGE, $_CONFIG;
+
+        $_STORAGE['database']->db_connect(
+            array(
+                'host' => $_CONFIG->host, 'dbname' => $_CONFIG->dbname,
+                'port' => $_CONFIG->port, 'user' => $_STORAGE['login'],
+                'password' => $_STORAGE['password']
+            )
+        );
+    }
+
+    public function test_sql() {
         global $_STORAGE;
+        // TODO maybe connect to file instead of database??
+
         $query = 'select version()';
-        $sql  = new SQLBase($query);
+        $sql = new SQLBase($query);
         $this->assertNotNull($sql);
+
         $opt = $sql->GetOptions();
         $this->assertNotNull($opt);
-        $sql->BeforeQuery("12345");
+
+        $sql->BeforeQuery('12345');
         $this->assertNotNull($_STORAGE['pids']);
+
+        $this->connectDB();
+
         $sql->AfterQuery(0);
-        $res = $sql->ExistsScheme("meta");
+        $res = $sql->ExistsScheme('meta');
         $this->assertNotNull($res);
+
         $res = $sql->ProcessResult($_STORAGE['database']->db_query($query), 1);
         $this->assertNotNull($res[0]['version']);
-
     }
 }
