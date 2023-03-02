f3e249eaaa6a150a4def4f38d2df87aca4aa7220
Иван Пчелинцев <pchel.21@yandex.ru>
2023-03-02 13:29:31 +0000

    Update DbSqlController.php

--- Server/DbSqlController.php
+++ Server/DbSqlController.php
@@ -111,7 +111,10 @@ class DbSqlController
 
         $databaseObject->set_bytea_output();
         $databaseObject->set_interval_style();
-        $databaseObject->set_datestyle();
+        
+        if (!defined('PHPUNIT_COMPOSER_INSTALL') && !defined('__PHPUNIT_PHAR__')) {
+             $databaseObject->set_datestyle();
+        }
 
         $sqlObject->BeforeQuery($pid);
 
